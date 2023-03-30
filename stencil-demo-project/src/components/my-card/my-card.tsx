752cf4e29383851f8a834b46761e4fe42b17706e
Shubh <shubh.scb@gmail.com>
2023-03-30 20:33:23 +0530

    API fetch complete

--- stencil-demo-project/src/components/my-card/my-card.tsx
+++ stencil-demo-project/src/components/my-card/my-card.tsx
@@ -1,5 +1,8 @@
 import { Component, h, Prop, State, Watch } from "@stencil/core";
 
+// api key
+// 3PTP9MLITVHFAHW7
+
 
 @Component({
     tag: 'my-card',
@@ -11,17 +14,20 @@ import { Component, h, Prop, State, Watch } from "@stencil/core";
 export class MyCard {
  
     @Prop({mutable: true}) userName: string;
-    @State() APIData: string;
+    // @State() APIData: string;
     @State() showcard: boolean = true;
     @State() showReactTab: boolean = false;
     @State() showStencilTab: boolean = false;
 
+    @State() myStencilUsers: string
+    @State() myReactUsers: String
 
-    changeState() {
-        this.userName = "Krishang";
-        this.APIData = "This data is comeing from API"
-        this.showcard = !this.showcard
-    }
+
+    // changeState() {
+    //     this.userName = "Krishang";
+    //     // this.APIData = "This data is comeing from API"
+    //     this.showcard = !this.showcard
+    // }
 
     onContentChange(content: string) {
         if(content == 'reacttab') {
@@ -36,55 +42,96 @@ export class MyCard {
         }
     }
 
+    getStencilUsersFromApi() {
+        this.myStencilUsers = 'Loading data ...'
+        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+        .then((res) => res.json())
+        .then((parseRes) => {
+            let timeSeries = parseRes['Time Series (5min)']
+            let timeDateStencil = timeSeries['2023-03-29 18:20:00']
+            this.myStencilUsers =timeDateStencil['1. open']
+        })
+    }
+
+    getReactUserFromApi() {
+        this.myReactUsers = 'Loading ...';
+        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+        .then((res) => res.json())
+        .then((parseRes) => {
+            let timeSeries = parseRes['Time Series (5min)'];
+            let timeDataReact = timeSeries['2023-03-29 18:20:00']
+            this.myReactUsers = timeDataReact['5. volume']
+        })
+    }
+
+    fetchDataFromAPI(contentType: string) {
+        if(contentType == 'stencil') {
+            this.getStencilUsersFromApi();
+        } else {
+            this.getReactUserFromApi
+        }
+    }
+ 
     onUserInput(event: Event) {
         this.userName = (event.target as HTMLInputElement).value
     }
 
+    //  componentWillLoad() {
+    //     this.APIData = 'Loading...'
+    //     fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
+    //     .then((res) => res.json())
+    //     .then((parsedRes) => {
+    //         let metaData = parsedRes['Meta Data']
+    //         let timeDateStencil = metaData['1. Information']
+    //         this.APIData = timeDateStencil;
+    //     })
+    // }
+
     // lifecycle methods
 
     // when component connects to the dom
-    connectedCallback() {
-        console.log('connectedcallback')
-    }
+    // connectedCallback() {
+    //     console.log('connectedcallback')
+    // }
     // when component disconnects from the dom
-    disconnectedCallback() {
-        console.log('disconnectedcallback')
-    }
+    // disconnectedCallback() {
+    //     console.log('disconnectedcallback')
+    // }
 
     // This method is only called once
     // this is a good place to load data asyncronously
-    componentWillLoad() {
-        console.log('Componentwillload component is about to load')
-    }
+    // componentWillLoad() {
+    //     console.log('Componentwillload component is about to load')
+    // }
 
     // It is always recomended to make any rendered state updates
     // within componentWillRender()
     // this.APIdata = 'updated'
-    componentWillRender() {
-        console.log('componentWillRender')
-    }
+    // componentWillRender() {
+    //     console.log('componentWillRender')
+    // }
 
 
     // called just once after component fully loaded
     // and the forst render() occurs
-    componentDidLoad() {
-        console.log('componentdidload')
-    }
+    // componentDidLoad() {
+    //     console.log('componentdidload')
+    // }
 
     // This hook is called when a Prop or State property changes
     // and a render is about to be requested. If this method return 
     // false the the component will not update anymore.
-    componentShouldUpdate() {
-        return true;
-    }
+    // componentShouldUpdate() {
+    //     return true;
+    // }
 
-    componentWillUpdate() {
-        console.log('componentwillupadte')
-    }
+    // componentWillUpdate() {
+    //     console.log('componentwillupadte')
+    // }
 
-    componentDidUpdate() {
-        console.log('componentdidupdate')
-    }
+    // componentDidUpdate() {
+    //     console.log('componentdidupdate')
+    // }
   
 
 
@@ -100,18 +147,18 @@ export class MyCard {
         let reactContent = (
              <div>
                 <div class="card-custom" id="react-div">
-                    Hello, From React <br></br> Live Users
-                    <button class="btn-react small-btn" onClick={this.changeState.bind(this)}>Get React Users</button><br></br>
+                    Hello, From React <br></br> Live Users <span>{this.myReactUsers}</span>
+                    <button class="btn-react small-btn" onClick={this.getReactUserFromApi.bind(this, 'react')} >Get React Users</button><br></br>
                 </div>
             </div>
         );
         let stencilContent = (
             <div>
                 <div class="card-custom" id="stencil-div">
-                    Hello, from Stencil
+                    Hello, from Stencil <span>{this.myStencilUsers}</span>
                     <br /> <br />
                     Live Users
-                    <button class="btn-stencil small-btn">Get Stencil Users</button> <br />
+                    <button class="btn-stencil small-btn" onClick={this.getStencilUsersFromApi.bind(this, 'stencil')}>Get Stencil Users</button> <br />
                     <br /><br />
                 </div>
             </div>
@@ -121,7 +168,7 @@ export class MyCard {
         let mainContent = (
             <div class="may-card-wrapper">
                 <h1>Hi, I am {this.userName}</h1>
-                <h5>{this.APIData}</h5>
+                {/* <h5>{this.APIData}</h5> */}
                 <button class="btn-stencil" onClick={this.onContentChange.bind(this, 'stenciltab')}>Stencil</button>
                 <button class="btn-react" onClick={this.onContentChange.bind(this, 'reacttab')}>React</button>
                 {this.showReactTab && reactContent}
